74ad8089e86d6d1f4c2a2e844240dfb70465136e
Adrien Hecq <adrien.hecq@stellar.io>
2022-08-29 16:33:33 +0200

    feat: second entity Books

---
+++ migrations/Version20220829141936.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220829141936 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description CLOB DEFAULT NULL, number_of_pages INTEGER NOT NULL, date_of_release DATETIME NOT NULL, update_date_of_release DATETIME DEFAULT NULL, author VARCHAR(255) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE books');
+    }
+}
---
+++ src/Entity/Books.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\BooksRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BooksRepository::class)]
+#[ApiResource(
+    itemOperations: [
+        "get"=>["path"=>"/getMyBook/{id}"],
+        "put"
+    ],
+    shortName: "book"
+)]
+class Books
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $numberOfPages = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateOfRelease = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updateDateOfRelease = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getNumberOfPages(): ?int
+    {
+        return $this->numberOfPages;
+    }
+
+    public function setNumberOfPages(int $numberOfPages): self
+    {
+        $this->numberOfPages = $numberOfPages;
+
+        return $this;
+    }
+
+    public function getDateOfRelease(): ?\DateTimeInterface
+    {
+        return $this->dateOfRelease;
+    }
+
+    public function setDateOfRelease(\DateTimeInterface $dateOfRelease): self
+    {
+        $this->dateOfRelease = $dateOfRelease;
+
+        return $this;
+    }
+
+    public function getUpdateDateOfRelease(): ?\DateTimeInterface
+    {
+        return $this->updateDateOfRelease;
+    }
+
+    public function setUpdateDateOfRelease(?\DateTimeInterface $updateDateOfRelease): self
+    {
+        $this->updateDateOfRelease = $updateDateOfRelease;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
