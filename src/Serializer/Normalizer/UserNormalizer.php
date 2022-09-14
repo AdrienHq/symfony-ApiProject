574748affb6447bbde20001b82e3ade98771d23b
AdrienHq <adrienhecq@gmail.com>
2022-09-14 07:51:09 +0200

    feat: User normalizer Logic WIP

--- src/Serializer/Normalizer/UserNormalizer.php
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -4,21 +4,26 @@ namespace App\Serializer\Normalizer;
 
 use App\Entity\User;
 use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
-class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface, NormalizerAwareInterface
 {
-    public function __construct(private ObjectNormalizer $normalizer)
-    {
-    }
+    use NormalizerAwareTrait;
+
+    private const ALREADY_CALLED = 'USER_NORMALIZER_ALREADY_CALLED';
 
     public function normalize($object, string $format = null, array $context = []): array
     {
+        /** @var User $object */
         if ($this->userIsOwner($object)) {
             $context['groups'][] = 'owner:read';
         }
-        /** @var User $object */
+
+        $context[self::ALREADY_CALLED] = true;
+
         $data = $this->normalizer->normalize($object, $format, $context);
 
         // TODO: add, edit, or delete some data
@@ -28,16 +33,26 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
 
     public function supportsNormalization($data, string $format = null, array $context = []): bool
     {
+        if(isset($context[self::ALREADY_CALLED])){
+            return false;
+        }
+
         return $data instanceof User;
     }
 
     public function hasCacheableSupportsMethod(): bool
     {
-        return true;
+        return false;
     }
 
     private function userIsOwner(User $user): bool
     {
-        rand(0, 10) > 5;
+        //todo
+        return rand(0, 10) > 5;
+    }
+
+    public function setNormalizer(NormalizerInterface $normalizer)
+    {
+        // TODO: Implement setNormalizer() method.
     }
 }
