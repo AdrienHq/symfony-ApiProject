3505c5d737523626990f5fc19a016e034a578488
AdrienHq <adrienhecq@gmail.com>
2022-09-14 08:55:13 +0200

    feat: Validator

---
+++ src/Validator/IsValidOwnerValidator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class IsValidOwnerValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        /* @var IsValidOwner $constraint */
+
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ value }}', $value)
+            ->addViolation();
+    }
+}
