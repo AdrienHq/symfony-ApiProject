fcb78f37ffad672f07e46fcb9a7ed285d228e2c3
AdrienHq <adrienhecq@gmail.com>
2022-09-15 08:50:34 +0200

    feat: Custom logic in persister

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -2,32 +2,42 @@
 
 namespace App\DataPersister;
 
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 use ApiPlatform\Core\DataPersister\DataPersisterInterface;
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\This;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserDataPersister implements DataPersisterInterface
+class UserDataPersister implements ContextAwareDataPersisterInterface
 {
     private $dataPersister;
     private UserPasswordHasherInterface $passwordHasher;
+    private LoggerInterface $logger;
 
-    public function __construct(DataPersisterInterface $dataPersister, UserPasswordHasherInterface $passwordHasher)
+    public function __construct(DataPersisterInterface $dataPersister, UserPasswordHasherInterface $passwordHasher, LoggerInterface $logger)
     {
         $this->dataPersister = $dataPersister;
         $this->passwordHasher = $passwordHasher;
+        $this->logger = $logger;
     }
 
-    public function supports($data): bool
+    public function supports($data, array $context = []): bool
     {
         return $data instanceof User;
     }
 
     /** @var User $data */
-    public function persist($data)
+    public function persist($data, array $context = [])
     {
-        if($data->getPlainPassword()){
+        if (($context['item_operation_name'] ?? null) === 'put') {
+            $this->logger->info(sprintf('User %s is being updated!', $data->getId()));
+        }
+
+        if (!$data->getId()) {
+            $this->logger->info(sprintf('User %s just registered!', $data->getEmail()));
+        }
+
+        if ($data->getPlainPassword()) {
             $data->setPassword(
                 $this->passwordHasher->hashPassword($data, $data->getPlainPassword())
             );
@@ -37,7 +47,7 @@ class UserDataPersister implements DataPersisterInterface
         $this->dataPersister->persist();
     }
 
-    public function remove($data)
+    public function remove($data, array $context = [])
     {
         $this->dataPersister->persist($data);
     }
